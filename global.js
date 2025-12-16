9b84b3f6d22979971c118d9297e97e7885faf559
susangreydev <susangreydev@gmail.com>
2025-12-16 02:32:05 -0500

    Update global.js

--- global.js
+++ global.js
@@ -1,5 +1,10 @@
-document.getElementById("contact_button").addEventListener("click",function() {
+function emailScript() {
+
+    navigator.clipboard.writeText("contact@susangrey.com").then(alert("⚠️ CONTACT@SUSANGREY.COM COPIED TO CLIPBOARD ⚠️"));
 
     window.location.href("mailto:contact@susangrey.com");
 
-});
+}
+
+document.getElementById("contact_button").addEventListener("click", emailScript);
+document.getElementById("profile_email_icon").addEventListener("click", emailScript);
